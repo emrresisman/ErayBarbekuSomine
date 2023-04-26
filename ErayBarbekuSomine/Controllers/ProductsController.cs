48b99aff422a372863fcc66922062707eb6a0384
Emre Şişman <emrre98@gmail.com>
2023-04-26 19:33:45 +0300

    update

---
+++ ErayBarbekuSomine/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ErayBarbekuSomine.Controllers
+{
+	public class ProductsController : Controller
+	{
+		public IActionResult DuzCepheliSomine()
+		{
+			return View();
+		}
+		public IActionResult KoseSomine()
+		{
+			return View();
+		}
+		public IActionResult LTipiSomine()
+		{
+			return View();
+		}
+
+		public IActionResult TruvaBarbeku()
+		{
+			return View();
+		}
+		public IActionResult TruvaCiftTarafli()
+		{
+			return View();
+		}
+		public IActionResult TruvaTekTarafli()
+		{
+			return View();
+		}
+		public IActionResult UTipiSomine()
+		{
+			return View();
+		}
+	}
+}
